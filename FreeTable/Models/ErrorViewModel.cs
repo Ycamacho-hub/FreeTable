28ba5231c6981889c556232607e45df07fbfa218
Yostin Camacho <yosarca@gmail.com>
2025-02-07 14:29:21 +0100

    Agregar archivos de proyecto.

---
+++ FreeTable/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace FreeTable.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
